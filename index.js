08926218cdbd5d6e60befd3d9428327314881dfb
davi-pou07 <davihareis@gmail.com>
2020-12-16 00:51:13 -0300

    nit commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+
+//view engine
+app.set('view engine','ejs')
+
+//static
+app.use(express.static('public'))
+
+//bodyparser - Para manipulação de formulario e json
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.listen(8080,()=>{
+    console.log("Server ligado")
+})
